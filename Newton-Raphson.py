1c09c3e8c9b55b6dc4c233105607c30270c62da9
reutavitan <73298850+reutavitan@users.noreply.github.com>
2022-02-21 19:18:21 +0200

    Update Newton-Raphson.py
    xxx

--- Newton-Raphson.py
+++ Newton-Raphson.py
@@ -1,16 +1,17 @@
 import sympy as sp
 from sympy.utilities.lambdify import lambdify
+from datetime import datetime
 
 
 # Defining Function
 def f(x):
-    return x ** 3 - 5 * x - 9
+    return (sp.sin((2 * x ** 3) + (5 * x ** 2) - 6)) / (2 * sp.exp((-2) * x))
 
 
 # Defining derivative of function
 def g(z):
     x = sp.symbols('x')
-    f = x ** 3 + 2 * x + 5
+    f = (sp.sin((2 * x ** 3) + (5 * x ** 2) - 6)) / (2 * sp.exp((-2) * x))
     f_prime = f.diff(x)  # Derivation of  f by x
     f_prime = lambdify(x, f_prime)
     return f_prime(z)
@@ -27,24 +28,26 @@ def newtonRaphson(x0, e):
             break
 
         x1 = x0 - f(x0) / g(x0)
-        print('Iteration-%d, x1 = %0.6f and f(x1) = %0.6f' % (step, x1, f(x1)))
+        print('Iteration-%d, x1 = %0.6f and f(x1) = %0.6f' % (step, x1, f(x1))+"00000"+d+h+m)
         x0 = x1
         step = step + 1
 
         condition = abs(f(x1)) > e
 
-    print('\nRequired root is: %0.8f' % x1)
+    print('\nRequired root is: %0.8f' % x1+"00000"+d+h+m)
 
-
+local_dt = datetime.now()
+d = str(local_dt.day)
+h = str(local_dt.hour)
+m = str(local_dt.minute)
 # Input Section
-# poly = input('Enter Polynomial:')
-x1 = int(input('Enter start Range: '))
-x2 = int(input('Enter end Range: '))
-x0 = (x2 - x1) / 2
-
+# Function sin((2 * x ** 3) + (5 * x ** 2) - 6)) / (2 * sp.exp((-2) * x))
+x1 = float(input('Enter start Range: '))
+x2 = float(input('Enter end Range: '))
+x0 = 0.05
 # Converting x0
 x0 = float(x0)
 e = float(0.00001)
-
 # Starting Newton Raphson Method
 newtonRaphson(x0, e)
+
